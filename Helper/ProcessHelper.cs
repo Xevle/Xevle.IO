22c160f2bd1da4d6e544b6fa6c8e8951614b6eed
seeseekey <kontakt@seeseekey.net>
2016-09-26 19:09:16 +0200

    Add process helper class.

---
+++ Helper/ProcessHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Xevle.IO.Helper
+{
+	public static class ProcessHelper
+	{
+		public static bool StartProcess(string filename, string arguments = "", bool waitForExit = false)
+		{
+			try
+			{
+				Process process = new Process();
+
+				process.EnableRaisingEvents = false;
+				process.StartInfo.FileName = filename;
+				process.StartInfo.Arguments = arguments;
+
+				process.Start();
+				if (waitForExit) process.WaitForExit();
+			}
+			catch
+			{
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
